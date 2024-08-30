73cf2fbf277621fddbd206a3ce2602b010689ebe
pacarti <a.paciorkowski@protonmail.com>
2024-08-30 14:29:54 +0200

    Replaced os.walk with os.listdir since I don't need to walk through subdirectories.

--- mergeVideos.py
+++ mergeVideos.py
@@ -9,7 +9,7 @@ def isFilenameValid(videoFileName):
     if properFileNameRegex.match(videoFileName):
         return True
 
-import os, sys, re, subprocess
+import os, sys, re
 from pathlib import Path
 
 
@@ -20,16 +20,44 @@ os.chdir(videosDir)
 
 listFile = open('list.txt', 'w')
 
-for folderName, subfolders, filenames in os.walk(videosDir):
+mp4FilesCount = 0
+webmFilesCount = 0
+mkvFilesCount = 0
+
+# for folderName, subfolders, filenames in os.walk(videosDir):
     # Sort the filenames alphabetically:
-    filenames.sort()
-    for file in filenames:
-        if file.endswith('.mp4') or file.endswith('.webm') or file.endswith('.mkv'):
-            if isFilenameValid(file):
-                listFile.write('file ' + '\'' + file + '\'\n')
-                # print(file)
-            else:
-                print("The video file name can contain only A-Z, a-z, 0-9, '.', '-' or '_'. Please rename the file: " + file + ".")
-                exit()
+# filenames.sort()
+for file in os.listdir(videosDir):
+    if file.endswith('.mp4'):
+        if isFilenameValid(file):
+            listFile.write('file ' + '\'' + file + '\'\n')
+            mp4FilesCount += 1
+            # print(file)
+        else:
+            print("The script applies only to video files in formats: .mp4, .webm. and .mkv. The video file name can contain only A-Z, a-z, 0-9, '.', '-' or '_'.\nPlease rename the file: " + file + ".")
+            exit()
+
+    elif file.endswith('.webm'):
+        if isFilenameValid(file):
+            listFile.write('file ' + '\'' + file + '\'\n')
+            webmFilesCount += 1
+        else:
+            print("The script applies only to video files in formats: .mp4, .webm. and .mkv. The video file name can contain only A-Z, a-z, 0-9, '.', '-' or '_'.\nPlease rename the file: " + file + ".")
+            exit()
+    elif file.endswith('.mkv'):
+        if isFilenameValid(file):
+            listFile.write('file ' + '\'' + file + '\'\n')
+            mkvFilesCount += 1
+        else:
+            print("The script applies only to video files in formats: .mp4, .webm. and .mkv. The video file name can contain only A-Z, a-z, 0-9, '.', '-' or '_'.\nPlease rename the file: " + file + ".")
+            exit()
+        
+if mp4FilesCount > 0 and webmFilesCount == 0 and mkvFilesCount == 0:
+    print("Only MP4 files.")
+elif webmFilesCount > 0 and mp4FilesCount == 0 and mkvFilesCount == 0:
+    print("Only WEBM files.")
+elif mkvFilesCount > 0 and mp4FilesCount == 0 and webmFilesCount == 0:
+    print("Only MKV files.")
+
 
 # After the proper list.txt file is created, the rest(ffmpeg command) is executed in the shell script.
