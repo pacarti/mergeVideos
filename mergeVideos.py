05862dd980bb24982925d5ee80a192184e2adbd2
pacarti <a.paciorkowski@protonmail.com>
2024-09-08 01:10:03 +0200

    Added the functionality of executing conversion non-mp4 files to mp4 if the majority of files is mp4.

--- mergeVideos.py
+++ mergeVideos.py
@@ -9,7 +9,7 @@ def isFilenameValid(videoFileName):
     if properFileNameRegex.match(videoFileName):
         return True
 
-import os, sys, re
+import os, sys, re, subprocess
 from pathlib import Path
 
 
@@ -28,6 +28,7 @@ mkvFilesCount = 0
     # Sort the filenames alphabetically:
 # filenames.sort()
 for file in sorted(os.listdir(videosDir)):
+    # print(file)
     if file.endswith('.mp4'):
         if isFilenameValid(file):
             listFile.write('file ' + '\'' + file + '\'\n')
@@ -37,21 +38,21 @@ for file in sorted(os.listdir(videosDir)):
             print("The script applies only to video files in formats: .mp4, .webm. and .mkv. The video file name can contain only A-Z, a-z, 0-9, '.', '-' or '_'.\nPlease rename the file: " + file + ".")
             exit()
 
-    elif file.endswith('.webm'):
+    if file.endswith('.webm'):
         if isFilenameValid(file):
-            file.sort()
             listFile.write('file ' + '\'' + file + '\'\n')
             webmFilesCount += 1
         else:
             print("The script applies only to video files in formats: .mp4, .webm. and .mkv. The video file name can contain only A-Z, a-z, 0-9, '.', '-' or '_'.\nPlease rename the file: " + file + ".")
             exit()
-    elif file.endswith('.mkv'):
+    if file.endswith('.mkv'):
         if isFilenameValid(file):
             listFile.write('file ' + '\'' + file + '\'\n')
             mkvFilesCount += 1
         else:
             print("The script applies only to video files in formats: .mp4, .webm. and .mkv. The video file name can contain only A-Z, a-z, 0-9, '.', '-' or '_'.\nPlease rename the file: " + file + ".")
-            sys.exit(0)
+            # sys.exit(0)
+            exit()
         
 if mp4FilesCount > 0 and webmFilesCount == 0 and mkvFilesCount == 0:
     # print("Only MP4 files.")
@@ -63,10 +64,24 @@ elif mkvFilesCount > 0 and mp4FilesCount == 0 and webmFilesCount == 0:
     # print("Only MKV files.")
     sys.exit(3)
 elif mp4FilesCount > webmFilesCount and mp4FilesCount > mkvFilesCount:
-    print("Majority MP4 files.")
+    # print("Majority MP4 files.")
+    # If the majority is MP4, convert the rest to mp4
+    # os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    for file in sorted(os.listdir(videosDir)):
+            if file.endswith('.webm') or file.endswith('.mkv'):
+                subprocess.run([os.path.dirname(os.path.abspath(__file__)) + "/./convertToMp4.sh", file])
+            # sys.exit(1)
 elif webmFilesCount > mp4FilesCount and webmFilesCount > mkvFilesCount:
-    print("Majority WEBM files.")
+    # print("Majority WEBM files.")
+    # If the majority is WEBM, convert the rest to webm
+    # for file in sorted(os.listdir(videosDir)):
+    #     if not file.endswith('.webm'):
+    sys.exit(2)
 elif mkvFilesCount > webmFilesCount and mkvFilesCount > mp4FilesCount:
-    print("Majority MKV files.")
+    # print("Majority MKV files.")
+    # If the majority is MKV, convert the rest to mkv
+    # for file in sorted(os.listdir(videosDir)):
+    #     if not file.endswith('.mkv'):
+    sys.exit(3)
 
 # After the proper list.txt file is created, the rest(ffmpeg command) is executed in the shell script.
