b33453f63c6a3e75b6bc77222cbaffe26d8b8c90
pacarti <a.paciorkowski@protonmail.com>
2024-09-09 00:28:42 +0200

    removed '-y' from the command so that when there is a name conflict, ffmpeg will ask user if he wants to overwrite the file

--- convertToMp4.sh
+++ convertToMp4.sh
@@ -9,4 +9,4 @@ fileNoExt="${file%.*}"
 # echo "file without extension: $fileNoExt"
 # echo "file with MP4 extension: $fileNoExt.mp4"
 
-ffmpeg -y -i "$file" -c:v libx264 -c:a aac "$fileNoExt.mp4"
+ffmpeg -i "$file" -c:v libx264 -c:a aac "$fileNoExt.mp4"
